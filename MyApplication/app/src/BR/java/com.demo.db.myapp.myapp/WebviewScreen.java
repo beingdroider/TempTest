259a3b24f57d26dacd652e9ed15c17cc6fbb11d5
Being Droider <being.droider@gmail.com>
2016-01-29 12:57:36 -0600

    dummy

--- MyApplication/app/src/BR/java/com.demo.db.myapp.myapp/WebviewScreen.java
+++ MyApplication/app/src/BR/java/com.demo.db.myapp.myapp/WebviewScreen.java
@@ -23,7 +23,7 @@ public class WebviewScreen extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "BR button actions", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
